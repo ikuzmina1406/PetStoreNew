593641df30b7406d307a0d29490500de1741983c
ikuzmina1406 <ikuzmina1406@gmail.com>
2022-05-24 00:50:00 +0300

    Added class ApiResponse

---
+++ src/main/java/ua/goIt/model/ApiResponse.java
@@ -0,0 +1,46 @@
+package ua.goIt.model;
+
+public class ApiResponse {
+    private Long code;
+    private String type;
+    private String message;
+
+    public Long getCode() {
+        return code;
+    }
+
+    public void setCode(Long code) {
+        this.code = code;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public ApiResponse(Long code, String type, String message) {
+        this.code = code;
+        this.type = type;
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "ApiResponse{" +
+                "code=" + code +
+                ", type='" + type + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
